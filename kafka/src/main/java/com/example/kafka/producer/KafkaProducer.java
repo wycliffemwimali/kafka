da88c9dc9779533094cbb26c4f740f719b102136
wycliffemwimali <wycliffemwimali2@gmail.com>
2024-05-28 21:17:28 +0300

    kafka producer

---
+++ kafka/src/main/java/com/example/kafka/producer/KafkaProducer.java
@@ -0,0 +1,20 @@
+package com.example.kafka.producer;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+import static java.lang.String.format;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class KafkaProducer {
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    public void sendMessage(String message) {
+        log.info(format("Producing message: %s", message));
+        kafkaTemplate.send("wycliffe", message);
+    }
+}
